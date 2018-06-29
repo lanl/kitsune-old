9388c323aa142aeb7ed6faa7189d8886988bf8c4
Nick Moss <nickm@lanl.gov>
2018-06-29 13:36:38 -0600

    apply automatic linking of kitsune compiler RT to other platforms

--- llvm/tools/kitsune/clang/lib/Driver/ToolChains/FreeBSD.cpp
+++ llvm/tools/kitsune/clang/lib/Driver/ToolChains/FreeBSD.cpp
@@ -294,8 +294,13 @@ void freebsd::Linker::ConstructJob(Compilation &C, const JobAction &JA,
   }
 
   // +===== Kitsune
-  if (D.CCCIsFleCSI() || D.CCCIsKokkos())
+  if (D.CCCIsFleCSI() || D.CCCIsKokkos()) {
     CmdArgs.push_back("-lcilkrts");
+    if (Args.hasArg(options::OPT_fgpu)) {
+      CmdArgs.push_back("-lcuda");
+      ToolChain.getCompilerRTArgString(Args, "kitsune", false);
+    }
+  }
   // ==============
 
   if (!Args.hasArg(options::OPT_nostdlib, options::OPT_nostartfiles)) {
