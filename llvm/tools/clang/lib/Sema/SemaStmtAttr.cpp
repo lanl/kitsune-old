d4d21a0b5fb8d730347906e045fe42246593bda8
Amalee Wilson <amaleewilson@lanl.gov>
2018-09-27 17:20:26 -0600

    Added some things that were missing for pipevec

--- llvm/tools/clang/lib/Sema/SemaStmtAttr.cpp
+++ llvm/tools/clang/lib/Sema/SemaStmtAttr.cpp
@@ -196,8 +196,6 @@ static Attr *handlePvHintAttr(Sema &S, Stmt *St, const AttributeList &A,
   Expr *BufferValue = A.getArgAsExpr(5);
   Expr *ListValue = A.getArgAsExpr(6);
 
-
-
   bool PragmaPv = PragmaNameLoc->Ident->getName() == "pipevec";
   if (St->getStmtClass() != Stmt::DoStmtClass &&
       St->getStmtClass() != Stmt::ForStmtClass &&
@@ -366,6 +364,7 @@ static Attr *handleOpenCLUnrollHint(Sema &S, Stmt *St, const AttributeList &A,
 
 static Attr *ProcessStmtAttribute(Sema &S, Stmt *St, const AttributeList &A,
                                   SourceRange Range) {
+
   switch (A.getKind()) {
   case AttributeList::UnknownAttribute:
     S.Diag(A.getLoc(), A.isDeclspecAttribute() ?
@@ -393,6 +392,7 @@ static Attr *ProcessStmtAttribute(Sema &S, Stmt *St, const AttributeList &A,
 
 StmtResult Sema::ProcessStmtAttributes(Stmt *S, AttributeList *AttrList,
                                        SourceRange Range) {
+
   SmallVector<const Attr*, 8> Attrs;
   for (const AttributeList* l = AttrList; l; l = l->getNext()) {
     if (Attr *a = ProcessStmtAttribute(*this, S, *l, Range))
