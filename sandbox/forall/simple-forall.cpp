f0293e68df8054990da6750516cec0878e6bf4a6
Pat McCormick <pat@lanl.gov>
2019-02-25 15:35:48 -0700

    Tweaking sandbox for a bit easier testing.

--- sandbox/forall/simple-forall.cpp
+++ sandbox/forall/simple-forall.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 #include <stdlib.h>
 
+#ifndef TAPIR_STRATEGY
+#define TAPIR_STRATEGY "seq"
+#endif 
+
 int main(int argc, char *argv[]) {
   int N = argc > 1 ? atoi(argv[1]) : 100; 
   if (N < 10) 
     N = 10;
   double *a = new double(N);
 
-  [[tapir::strategy(STRATEGY)]]
+  [[tapir::strategy(TAPIR_STRATEGY)]]
   forall(int i = 0; i < N; i++) {
     a[i] = double(i);
   }
 
-  [[tapir::strategy(STRATEGY)]]
+  [[tapir::strategy(TAPIR_STRATEGY)]]
   forall(int i = 0; i < 10; i++) {
     cout << "a[" << i << "] = " << a[i] <<  endl;
   }
